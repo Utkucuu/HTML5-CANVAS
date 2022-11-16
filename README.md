368cf6fba612c90c5c95795b24a5ba740bc5e9b1
utku <bayraktarogluutku@gmail.com>
2022-11-16 16:03:30 +0300

    düzeltmeler yapıldı, myCanvas2 bitti

--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 ### Canvas Çalışma Notları
 
+##### HTML
+- Canvas boyutu css kullanmadan verilir
+```<canvas id="myCanvas" height="200" width="300"></canvas>```
+
+##### #myCanvas
 - Canvas ı seçtik
 ```var firstCanvas = document.getElementById("myCanvas")``` 
 
 - Kaç boyutlu çizim yapacağımızı belirttik.
 ```firstCanvas.getContext("2d")```  
 
-- Canvas içindebir nesne oluşturmak.
+- Canvas içinde bir şekil oluşturacağımızı söyledik.
 ``` var ctx = firstCanvas.getContext("2d") ```
 
 -Renk komutu önce verilmeli 
 ```ctx.fillStyle="red"```
 
-- fillRect fonksiyonu nesnemizi 4 köşali hale getirip özelliklerini vermemizi sağlar.
+- fillRect fonksiyonu şeklimizi 4 köşali hale getirip özelliklerini vermemizi sağlar.
 ```ctx.fillRect(x,y,widht,height)```
 <img src="/images/images1.png" alt="" width="300" height="300">
+
+***
+
+##### #myCanvas2
+- Aşağıdaki gibi valueX veya valueY gibi değişkenler kullanılarak koşul durumları yönetilebilir ve döngülerle örüntüler oluşturulabilir.
+```  utku.fillRect(x, y, h, w)
+        x += valueX
+        y += valueY```
--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 
-window.onload=function(){
+window.onload = function () {
+
+    /********************* #myCanvas ********************* */
 
     var firstCanvas = document.getElementById("myCanvas")
-
-   var ctx = firstCanvas.getContext("2d") 
-
+    var ctx = firstCanvas.getContext("2d")
     // ctx.fillRect(x,y,widht,height)
+    ctx.fillStyle = "red"
+    ctx.fillRect(50, 25, 100, 100)
 
-    ctx.fillStyle="red"
+    /********************* #myCanvas2 ********************** */
+    var myCanvas2 = document.getElementById("myCanvas2")
 
-    ctx.fillRect(50,25,100,100)
-    
+    var utku = myCanvas2.getContext("2d")
+
+    x = 200
+    y = 40
+    h = 15
+    w = 15
+    valueX = 20
+    valueY = 13
+
+    for (let index = 0; index < 33; index++) {
+
+        utku.fillRect(x, y, h, w)
+        x += valueX
+        y += valueY
+
+        if (index > 6) {
+            valueX = -20
+        }
+
+        if (index > 14) {
+            valueY = -13
+        }
+
+        if (index > 22) {
+            valueY = -13
+            valueX = 20
+        }
 
 
-
+    }
 }
